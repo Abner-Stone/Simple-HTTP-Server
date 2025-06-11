19a91981f4385f352430172f285030931f293872
Abner-Stone <abnerfstone2@gmail.com>
2025-06-11 05:23:45 -0500

    Remove changes

--- src/main.c
+++ src/main.c
@@ -54,13 +54,8 @@ int main() {
   printf("Waiting for a client to connect...\n");
   client_addr_len = sizeof(client_addr);
 
-  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
   printf("Client connected\n");
-  printf("Client_fd: %d\n", client_fd);
-  if (client_fd < 0) {
-    printf("Accept failed: %s \n", strerror(errno));
-    return 1;
-  }
 
   close(server_fd);
 
